94bec76e797705594c2c04c05892a7282faa76dc
Si Thu HLAING <fumaingqiang@gmail.com>
2019-11-29 16:36:27 +0700

    exe 4

---
+++ src/views/Exercise4.vue
@@ -0,0 +1,35 @@
+<template>
+  <div  id="exercise">
+    <div >
+      <button >Start Effect</button>
+      <div  id="effect" class="shrink"></div>
+    </div>
+    <div  class="float blue text-color">I got no class :(</div>
+    <div >
+      <input  type="text" />
+      <div  class="visible"></div>
+    </div>
+    <div >
+      <input  type="text" />
+      <input  type="text" />
+      <div  class></div>
+    </div>
+    <div >
+      <input  type="text" />
+      <div  style="width: 100px; height: 150px; background-color: gray;"></div>
+    </div>
+    <div >
+      <button >Start Progress</button>
+      <div  class="progress-bar" style="width: 0px; background-color: red;"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+  },
+  watch: {
+  }
+};
+</script>
